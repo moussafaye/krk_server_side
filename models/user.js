fe6b1db4060f180483f2b4d6ae03d87bbc5f7777
moussafaye <107629046+moussafaye@users.noreply.github.com>
2022-07-06 16:16:28 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,11 +5,7 @@ const userSchema = mongoose.Schema({
   fname: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  phoneNumber: { type: Number, required: true },
-  matiere: { type: String, required: true },
-  domaine: { type: String, required: true },
-  honoraire: { type: Number, required: true },
-  niveau: { type: String, required: true },
+ 
   id: { type: String },
 });
 
